58ac763ee07576d008ca744314d4196ebf516356
Josh Hayes <jhayes0128@gmail.com>
2018-03-25 18:20:52 -0500

    Fixed and finished error. Added Welcom screen

--- main.py
+++ main.py
@@ -7,27 +7,30 @@ app.config['DEBUG'] = True
 form = """
 <!doctype html>
 <html>
+    <head>
     <title>User Signup</title>
+        <style>.error_message {{color: red}}</style>
+    </title>
     <body>
-        <form action="/welcome" method="POST">
+        <form action="/" method="POST"> <!--change to /welcome -->
             <h1>Signup</h1>
             <label for="username">Username</label>
                 <input type="text" name="username" value="{username}"/>
-                <span>{username_error}</span>
+                <span class="error_message">{username_error}</span>
                 <br>
             <label for="password">Password</label>
                 <input type="password" name="password" />
-                <span>{password_error}</span>
+                <span class="error_message">{password_error}</span>
                 <br>
             <label for="verify_password">Verify Password</label>
                 <input type="password" name="verify_password" />
-                <span>{verify_password_error}</span>
+                <span class="error_message">{verify_password_error}</span>
                 <br>
             <label for="email">Email (optional)</label>
                 <input type="text" name="email" value="{email}" />
-                <span>{email_error}</span>
+                <span class="error_message">{email_error}</span>
                 <br>
-            <input type="submit" />
+            <input type="submit"/>
         </form>
     </body>
 </html>
@@ -43,7 +46,7 @@ def is_empty(string):    #to test if username/password/verify_password is empty
     else:
         return False
 
-def valid_parameters(string):    #checks if there is a space in the submission
+def valid_parameters(string):    #checks for spaces and length between 3 and 20 characters
     for i in string:
         if i == " ":
             return "Cannot contain space characters"
@@ -52,34 +55,12 @@ def valid_parameters(string):    #checks if there is a space in the submission
     else:
         return True
 
-def validate_email(string):    #checks email for spaces, number of @ and . characters, and length between 3 and 20
-    space_count = 0
-    at_symbol_count = 0
-    period_symbol_count = 0
-    for i in string:
-        if i == " ":
-            space_count += 1
-        if i == "@":
-            at_symbol_count += 1
-        if i == ".":
-            period_symbol_count += 1
-    if space_count > 0:
-        return "Cannot contain spaces"
-    elif at_symbol_count > 1:
-        return "Cannot contain more than one '@' symbol"
-    elif period_symbol_count > 1:
-        return "Cannot contain more than on '.' symbol"
-    elif len(string) < 3 or len(string) > 20:
-        return "Must be between 3 and 20 characters"
-    else:
-        return True
-
 @app.route("/", methods=["POST"])
 def validate_submission():
-    username = request.form("username")
-    password = request.form("password")
-    verify_password = request.form("verify_password")
-    email = request.form("email")
+    username = request.form["username"]
+    password = request.form["password"]
+    verify_password = request.form["verify_password"]
+    email = request.form["email"]
 
     username_error = ""
     password_error = ""
@@ -95,16 +76,50 @@ def validate_submission():
         verify_password_error = "Must re-type password"
 
     #check username/password for any spaces and length between 3 and 20 characters
-    if not valid_parameters(username):
-        username_error = valid_parameters(username)
-    if not valid_parameters(password):
-        password_error = valid_parameters(password)
+    for i in username:
+        if i == " ":
+            username_error = "Cannot contain space characters"
+    if len(username) < 3 or len(username) > 20:    #checks that the length of the submission is between 3 and 20 characters
+        username_error = "Must be between 3 and 20 characters"
+
+    for i in password:
+        if i == " ":
+            password_error = "Cannot contain space characters"
+    if len(password) < 3 or len(password) > 20:    #checks that the length of the submission is between 3 and 20 characters
+        password_error = "Must be between 3 and 20 characters"
 
     #checks if password and verify_password match
     if password != verify_password:
-        verify_password_error = "Passwords do no match"
+        verify_password_error = "Passwords do not match"
 
+    #checks email for > 1 @ or . symbol and length between 3 and 20
+    space_count = 0
+    at_symbol_count = 0
+    period_symbol_count = 0
+    for i in email:
+        if i == " ":
+            space_count += 1
+        if i == "@":
+            at_symbol_count += 1
+        if i == ".":
+            period_symbol_count += 1
+    if space_count > 0:
+        email_error = "Cannot contain spaces"
+    elif at_symbol_count > 1:
+        email_error = "Cannot contain more than one '@' symbol"
+    elif period_symbol_count > 1:
+        email_error = "Cannot contain more than on '.' symbol"
+    elif len(email) < 3 or len(email) > 20:
+        email_error = "Must be between 3 and 20 characters"
+
+    #either redirect to welcome page or display errors
+    if username_error == "" and password_error == "" and verify_password_error == "" and email_error == "":
+        return redirect("/welcome?u={0}".format(username))
+    else:
+        return form.format(username=username,email=email,username_error=username_error,password_error=password_error,verify_password_error=verify_password_error,email_error=email_error)
     
-
-
+@app.route('/welcome')
+def welcome():
+    u = request.args.get('u')
+    return "<h1>Welcome, {0}</h1>".format(u)
 app.run()
